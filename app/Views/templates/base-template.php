6353967d43f93cd2d81bf957e6659aeb84f09fe7
eterniteaa <imteeyaa31@gmail.com>
2022-09-10 14:56:01 +0800

    coba

--- app/Views/templates/base-template.php
+++ app/Views/templates/base-template.php
@@ -2,16 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $title; ?></title>
-  <link rel="stylesheet" href="<?= asset('css/app.css') ?>">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="<?= asset('css/app.css') ?>">
 </head>
 
 <body>
-  <h2>Something</h2>
-  <?= $this->renderSection("body") ?>
+    <h2>Something</h2>
+    <h1>Coba new</h1>
+    <?= $this->renderSection("body") ?>
 </body>
 
 </html>
